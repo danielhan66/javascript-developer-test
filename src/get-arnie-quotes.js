c16da18c3f72d51595a5af36bc7b918fda69d7f4
danielhan <daniel.han@ofx.com>
2022-03-02 06:56:07 +1100

    Implemented getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
+const getMessage = async (url) => {
+  const response = await httpGet(url);
+
+  const message = JSON.parse(response.body).message;
+
+  if(response.status === 200){
+    return { 'Arnie Quote': message };
+  }else{
+    return { 'FAILURE': message };
+  }
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  try{
+    const results = await Promise.all(urls.map(async (url) => {
+      return await getMessage(url);
+    }));
+
+    return results;
+  }catch(err){
+    console.error(err.message);
+    throw err
+  }
 };
 
 module.exports = {
